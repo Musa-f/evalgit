7cf726796b9dd9ce16fdba7627d92212affe8578
Musa-f <musaevafatima77@gmail.com>
2022-09-27 15:16:37 +0200

    c'est bon

---
+++ view/view_connexion.php
@@ -0,0 +1,17 @@
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<title>connexion:</title>
+</head>
+<body>
+<h1>connexion:</h1>
+<form action="" method="post">
+<p>veillez saisir votre identifiant:</p>
+<input type="text" name="login">
+
+<p>veuillez saisir votre mot de passe:</p>
+<input type="text" name="mdp">
+<input type="submit" value="connexion">
+</form>
+</body>
+</html>
